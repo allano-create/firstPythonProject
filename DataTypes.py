3684e266c8b60708491c9d22bd21dcccb3fec6a5
unknown <happytimesmemes@gmail.com>
2024-08-20 08:28:48 -0700

    first commit

---
+++ DataTypes.py
@@ -0,0 +1,29 @@
+#Python DataTypes
+#Numbers : We have Integers, float
+x = 5
+y = 10
+
+#print(x+y)
+#print(y-x)
+#float
+t = 2.5
+type(t)
+#print(type(t))
+#print(type(x))
+
+#Strings
+#FirstName = "Hallan"
+#MiddleName= "Chege"
+#Surname = "Warigia"
+#print(f"{FirstName} {MiddleName} {Surname}")
+#print(FirstName + " " + MiddleName + " " + Surname)
+
+#FirstName = input("Enter Your Firstname : ")
+#Surname = input("Enter Your Surname :")
+#print(f"Your firstname is {FirstName} and your last name is {Surname}")
+
+
+
+
+
+
---
+++ first.py
@@ -0,0 +1,22 @@
+#This code prints my personal information - This is a single line comment
+#The code talks more about bla bla...
+print("Hallan Chege Warigia")
+print(25)
+print("I love travelling")
+print("My favourite color is green")
+print("My Birthplace is Kenyanjeru")
+print("I'm a Senior Software Engineer")
+
+#This is my salary calculator
+# print(200000+500000)
+
+#variables -- container for storing data  -- pioneerSalary is our variable name - = is the assignment operator
+pioneerSalary = 200000
+googleSalary = 700000
+firstName = "Hallan"
+
+print(pioneerSalary+googleSalary)
+print(firstName)
+print(f"My name is {firstName} and my total salary is {pioneerSalary+googleSalary}")
+print(f"My name is {firstName} and my total salary is {pioneerSalary+googleSalary}")
+
